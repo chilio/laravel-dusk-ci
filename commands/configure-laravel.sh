c40d1cec69ffa8901e64fc8901eb6eac0e3bdf0c
Martijn Imhoff <info@martijnimhoff.nl>
2019-01-15 22:14:27 +0100

    Add creation of storage link to configure script (#37)
    This can be done in gitlab script. However, on the other hand, it should not introduce any problems, hence I am merging it. Thanks for pointing that out.

--- commands/configure-laravel.sh
+++ commands/configure-laravel.sh
@@ -4,6 +4,7 @@ chmod -R 775 storage
 chmod 775 bootstrap/cache
 chown -R www-data ./
 php artisan key:generate
+php artisan storage:link
 php artisan migrate:fresh --seed
 chrome-system-check
 sleep 10s
