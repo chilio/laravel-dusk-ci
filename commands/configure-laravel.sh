3f3687bf60124efce3c5cf1b045c68c2a710a40d
chilio <5613137+chilio@users.noreply.github.com>
2019-01-31 23:31:36 +0100

    Laravel 5.4 compatibility fix

--- commands/configure-laravel.sh
+++ commands/configure-laravel.sh
@@ -5,6 +5,10 @@ chmod 775 bootstrap/cache
 chown -R www-data ./
 php artisan key:generate
 php artisan storage:link
-php artisan migrate:fresh --seed
+if php artisan --version | grep "Laravel Framework 5.4."; then
+    php artisan migrate:refresh --seed
+else
+    php artisan migrate:fresh --seed
+fi
 chrome-system-check
 sleep 10s
