f455f9c6698f11496f56f341d0aacc845e74ee87
chilio <5613137+chilio@users.noreply.github.com>
2019-02-27 21:49:06 +0100

    Update chrome-system-check.sh

--- commands/chrome-system-check.sh
+++ commands/chrome-system-check.sh
@@ -18,5 +18,12 @@ if [ ${COMPATIBLE} == "INCOMPATIBLE" ]
 then
 exec chromedriver &
 printf "${GREEN}Starting inbuilt Chromedriver (${SYSTEMCHROMEDRIVER}).\n"
+printf "${YELLOW}Waiting for inbuilt Chromedriver to launch on port 9515...${NC}\n"
+
+while ! nc -z localhost 9515; do
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+
+printf "${GREEN}Chromedriver started succesfully.\n"
 fi
 printf "${NC}"
