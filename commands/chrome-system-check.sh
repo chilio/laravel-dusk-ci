b256b1fad0b0638748c4952fa4a9910b7984b16c
Patrik Freij <patrik.t.freij@gmail.com>
2019-03-19 11:15:16 +0100

    Run commands from current directory
    Instead of forcing commands to be from repository
    root, let's work from the current directory. This
    allows the laravel instance to reside in another
    directory within the project rather than always
    be in the root directory.

--- commands/chrome-system-check.sh
+++ commands/chrome-system-check.sh
@@ -9,7 +9,7 @@ printf "${GREEN}System Chromedriver version: ${NC}"
 SYSTEMCHROMEDRIVER=`chromedriver --version | grep -iEo -m 1 "[0-9]{1,3}.[0-9]{1,3}" | head -1`
 echo ${SYSTEMCHROMEDRIVER}
 printf "${GREEN}Laravel Chromedriver version: ${NC}"
-LARAVELCHROMEDRIVER=`${CI_PROJECT_DIR}/vendor/laravel/dusk/bin/chromedriver-linux --version | grep -iEo -m 1 "[0-9]{1,3}.[0-9]{1,3}" | head -1`
+LARAVELCHROMEDRIVER=`$(pwd)/vendor/laravel/dusk/bin/chromedriver-linux --version | grep -iEo -m 1 "[0-9]{1,3}.[0-9]{1,3}" | head -1`
 echo ${LARAVELCHROMEDRIVER}
 #LARAVELCHROMEDRIVER=2.37
 php /usr/bin/dusk-versions-check.php ${SYSTEMCHROME} ${SYSTEMCHROMEDRIVER} ${LARAVELCHROMEDRIVER} 1
--- commands/configure-laravel.sh
+++ commands/configure-laravel.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd ${CI_PROJECT_DIR}
 chmod -R 775 storage
 chmod 775 bootstrap/cache
 chown -R www-data ./
