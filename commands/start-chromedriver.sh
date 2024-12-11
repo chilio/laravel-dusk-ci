d3becbfb9eedd95b5e03c47758b1a128163cd9d2
Chilio <bart.goliszek@gmail.com>
2024-12-11 03:43:12 +0100

    Update

--- commands/start-chromedriver.sh
+++ commands/start-chromedriver.sh
@@ -6,5 +6,5 @@ while ! nc -z localhost 9515; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
 
-echo "Chromedriver started succesfully on port 9515"
+echo "Chromedriver initialized  succesfully on port=9515"
 
