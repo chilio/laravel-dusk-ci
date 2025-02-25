3eb9e70c50f52b6a55d47c2ea573def986474f15
chilio <5613137+chilio@users.noreply.github.com>
2025-02-26 00:32:14 +0100

    fixes to php-8.4 image

--- commands/chrome-system-check.sh
+++ commands/chrome-system-check.sh
@@ -11,7 +11,6 @@ echo ${SYSTEMCHROMEDRIVER}
 printf "${GREEN}Laravel Chromedriver version: ${NC}"
 LARAVELCHROMEDRIVER=`$(pwd)/vendor/laravel/dusk/bin/chromedriver-linux --version | grep -iEo -m 1 "[0-9]{1,3}.[0-9]{1,3}" | head -1`
 echo ${LARAVELCHROMEDRIVER}
-#LARAVELCHROMEDRIVER=2.37
 php /usr/bin/dusk-versions-check.php ${SYSTEMCHROME} ${SYSTEMCHROMEDRIVER} ${LARAVELCHROMEDRIVER} 1
 COMPATIBLE=`php /usr/bin/dusk-versions-check.php ${SYSTEMCHROME} ${SYSTEMCHROMEDRIVER} ${LARAVELCHROMEDRIVER} 0`
 if [ ${COMPATIBLE} == "INCOMPATIBLE" ]
@@ -20,14 +19,5 @@ if [ ${COMPATIBLE} == "INCOMPATIBLE" ]
 then
   printf "${GREEN}Starting compatible Chromedriver (${SYSTEMCHROMEDRIVER}).\n"
   source start-chromedriver
-#exec chromedriver &
-#printf "${GREEN}Starting inbuilt Chromedriver (${SYSTEMCHROMEDRIVER}).\n"
-#printf "${YELLOW}Waiting for inbuilt Chromedriver to launch on port 9515...${NC}\n"
-
-#while ! nc -z localhost 9515; do
-#  sleep 0.1 # wait for 1/10 of the second before check again
-#done
-
-#printf "${GREEN}Chromedriver started succesfully.${NC}\n"
 fi
 printf "${NC}"
