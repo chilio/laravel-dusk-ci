eaeda1bcbbe80c5e81bf52195757444d0f42f7a6
chilio <5613137+chilio@users.noreply.github.com>
2025-03-02 07:23:23 +0100

    updates & fixes

--- commands/dusk-versions-check.php
+++ commands/dusk-versions-check.php
@@ -34,6 +34,5 @@ if ($print_info_only) {
         print $yellow."You can read more, about running tests with systems inbuilt chromedriver in laravel-dusk-ci docs.".PHP_EOL;
     }
     print $nocolor;
-} else {
-    print ($compatible?"OK":"INCOMPATIBLE");
 }
+print ($compatible?"OK":"INCOMPATIBLE");
