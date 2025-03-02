38b8bd859ef8d1b6ef214db31053e0d78b753973
chilio <5613137+chilio@users.noreply.github.com>
2025-03-02 07:06:53 +0100

    updates & fixes

--- commands/start-chromedriver.sh
+++ commands/start-chromedriver.sh
@@ -11,5 +11,5 @@ while ! nc -z localhost 9515; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
 
-printf "${GREEN}Chromedriver initialized successfully on port 9515.${NC}\n "
+printf "${GREEN}System Chromedriver initialized successfully on port 9515.${NC}\n "
 
