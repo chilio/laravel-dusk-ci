5c5863b415277a1418724a10636e22a88cde364e
chilio <5613137+chilio@users.noreply.github.com>
2025-03-02 08:35:43 +0100

    updates & fixes

--- commands/chrome-system-check.sh
+++ commands/chrome-system-check.sh
@@ -11,7 +11,7 @@ SYSTEMCHROMEDRIVER=`chromedriver --version | grep -iEo -m 1 "[0-9]{1,3}.[0-9]{1,
 printf "${GREEN}Laravel Chromedriver version: ${NC}"
 LARAVELCHROMEDRIVER=`$(pwd)/vendor/laravel/dusk/bin/chromedriver-linux --version | grep -iEo -m 1 "[0-9]{1,3}.[0-9]{1,3}" | head -1`
 #echo ${LARAVELCHROMEDRIVER}
-php /usr/bin/dusk-versions-check.php ${SYSTEMCHROME} ${SYSTEMCHROMEDRIVER} ${LARAVELCHROMEDRIVER} 1
+# php /usr/bin/dusk-versions-check.php ${SYSTEMCHROME} ${SYSTEMCHROMEDRIVER} ${LARAVELCHROMEDRIVER} 1
 print "DEBUG: begin"
 COMPATIBLE=$(php /usr/bin/dusk-versions-check.php ${SYSTEMCHROME} ${SYSTEMCHROMEDRIVER} ${LARAVELCHROMEDRIVER} 0)
 print "DEBUG: $COMPATIBLE"
