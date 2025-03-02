3dbd8045bede9e14bfbecb749580c6da012aec9d
chilio <5613137+chilio@users.noreply.github.com>
2025-03-02 08:01:18 +0100

    updates & fixes

--- commands/chrome-system-check.sh
+++ commands/chrome-system-check.sh
@@ -14,7 +14,7 @@ echo ${LARAVELCHROMEDRIVER}
 php /usr/bin/dusk-versions-check.php ${SYSTEMCHROME} ${SYSTEMCHROMEDRIVER} ${LARAVELCHROMEDRIVER} 1
 COMPATIBLE=`php /usr/bin/dusk-versions-check.php ${SYSTEMCHROME} ${SYSTEMCHROMEDRIVER} ${LARAVELCHROMEDRIVER} 0`
 print "${COMPATIBLE}"
-if [${COMPATIBLE} == "INCOMPATIBLE"]; then
+if ["${COMPATIBLE}" == "INCOMPATIBLE"]; then
   printf "${GREEN}Starting system Chromedriver (${SYSTEMCHROMEDRIVER}).\n"
   source start-chromedriver
 else
